57350a1032057f92ff467672a090070c18ee3245
rcmoret <ryancmoret@gmail.com>
2012-10-02 11:06:03 -0500

    major refactoring

--- lib/cal_year.rb
+++ lib/cal_year.rb
@@ -1,6 +1,6 @@
 require 'calendar'
 
-class Entire_year < Individual_month
+class Entire_year < IndividualMonth
 
   def initialize(input_year)
     @year = input_year
@@ -20,12 +20,12 @@ class Entire_year < Individual_month
     single_month_array = []
     # This loop will create a instance of each month and place that month's lines into a master array
     months_iterations.each do |month|
-      this_instance = Individual_month.new(@year, month)
+      this_instance = IndividualMonth.new(@year, month)
       
-      single_month_array << this_instance.textual_representation_of_the_month.center(line_length)
+      single_month_array << this_instance.month_name_text.center(NUM_CHARS_IN_LINE)
       single_month_array << this_instance.build_array_of_individual_lines
       # This line will insure that all of the months have the same number of lines
-      single_month_array << (" " * 20) if single_month_array.size < 8
+      single_month_array << (" " * NUM_CHARS_IN_LINE) if single_month_array.size < 8
       
       master_array << single_month_array.flatten 
       single_month_array = [] # Reset this array before running through the loop again.
@@ -34,13 +34,13 @@ class Entire_year < Individual_month
   end
   
   def output_whole_year
-    lines = [] # This array will contain each line which can be output later
-    lines << "\n" + @year.to_s.center(64) + "\n"
+    line_array = [] # This array will contain each line which can be output later
+    line_array << "\n" + @year.to_s.center(64) + "\n"
     if @year == 1999 # This is an 'easter egg' (see http://en.wikipedia.org/wiki/Easter_egg_(media) fro more info)
       lines << "We're going to party like it's 1999!\n".center(64)
     end
     
-    whole_year_array = self.create_master_array
+    whole_year_array = create_master_array
     
     # Divide the calendar into quarters and group those months (which are arrays) into an array
     quarters = []
@@ -50,15 +50,15 @@ class Entire_year < Individual_month
     
     # For each quarter (array) loop thru and concatenate the lines from individual months and places those lines into the lines array
     quarters.each do |quarter|
-      output = String.new
+      single_string = ""
       line_index = 0 # This will be the outer loop
       month_index = 0 # This will be the inner loop. 
       while quarter[month_index][line_index]
-        output += quarter[month_index][line_index] + "  "
+        single_string += quarter[month_index][line_index] + "  "
         
         if month_index == 2 # Add to the array, increment line_index and reset month_index and string
-          lines << output
-          output = String.new
+          line_array << single_string
+          single_string = ""
           line_index += 1
           month_index = 0
         else # Just increment the month_index
@@ -67,8 +67,8 @@ class Entire_year < Individual_month
         
       end
     end
-    lines << "\n"
-    return lines
+    line_array << "\n"
+    return line_array
   end
 
 end
--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,19 +1,15 @@
-def print_week
-  print_week ="Su Mo Tu We Th Fr Sa"
-end
+WEEKDAYS_ABBREVIATED ="Su Mo Tu We Th Fr Sa"
 
-def line_length
-  line_length = print_week.length
-end
+NUM_CHARS_IN_LINE = WEEKDAYS_ABBREVIATED.length
 
-class Individual_month
+class IndividualMonth
   
   def initialize(year, month)
     @year = year
     @month = month
   end
   
-  def number_of_days_in_given_month
+  def number_days_in_a_month
     if [1, 3, 5, 7, 8, 10, 12].include?(@month)
       number_of_days = 31
     elsif[4, 6, 9, 11].include?(@month)
@@ -27,7 +23,7 @@ class Individual_month
     end
   end
 
-  def textual_representation_of_the_month
+  def month_name_text
     case @month
     when 1 
       "January"
@@ -58,77 +54,66 @@ class Individual_month
 
   # ZELLER'S CONGRUENCE (more info: http://en.wikipedia.org/wiki/Zeller%27s_congruence)
   # This algorithm will return the weekday as a digit for a date (given the month, day & year as variables)
-  def week_day_of_first_of_month
+  def weekday_of_1st_of_month
     day_of_month = 1 # This will be 1 representing the 1st of the month
     
     # Adjustments - Zeller's alg. treats Jan & Feb as the 13th & 14th months of prev. year
-
-    # Year
-    adjusted_year = @month <= 2 ? @year -1 : @year
-    
-    # Month
-    adjusted_month = @month <= 2 ? @month + 12 : @month
+    if @month <= 2
+      adjusted_year = @year -1
+      adjusted_month = @month + 12
+    else
+      adjusted_year = @year
+      adjusted_month = @month
+    end
     
     # Algorithm after adjustments.
     march_adjustment = ((adjusted_month + 1) * 2.6).floor
-    leap_year_offset = adjusted_year
-    leap_year_offset += (adjusted_year * 0.25).floor
+    leap_year_offset = (adjusted_year * 0.25).floor
     leap_year_offset += 6*((adjusted_year * 0.01).floor)
     leap_year_offset += (adjusted_year * 0.0025).floor
-    day_of_week = (day_of_month + march_adjustment + leap_year_offset) % 7
+    day_of_week = (day_of_month + adjusted_year + march_adjustment + leap_year_offset) % 7
     
     # Zeller's Congruence returns (0=Sat; 1 = Sun;...6=Fri)
     # For my purposes, it will be easier if an adjustment is made so  0 = Sun; 1 = Mon;...6 = Sat.
-    day_of_week != 0 ? week_day_of_first_of_month = day_of_week - 1 : week_day_of_first_of_month = 6
+    weekday_of_1st_of_month = day_of_week != 0 ?  day_of_week - 1 : 6
   end
 
-  def print_first_line_of_dates
-    output = String.new
-    
-    number_of_days_in_first_week = 7 - self.week_day_of_first_of_month
-    # This loop will add the day number for each day in the first week
-    day_number = 1 
-    while day_number <= number_of_days_in_first_week
-      output += day_number.to_s.rjust(2)
-      output += " " unless day_number == number_of_days_in_first_week
-      day_number += 1
-    end
-    return output.rjust(line_length)
+  def first_line_of_dates
+    number_of_days_in_first_week = 7 - weekday_of_1st_of_month
+    first_week_array = (1..number_of_days_in_first_week).to_a
+    output = first_week_array.join("  ").rjust(NUM_CHARS_IN_LINE)
   end
   
   def build_array_of_individual_lines
     individual_lines = []
     
     # Days of the Week
-    individual_lines << print_week
+    individual_lines << WEEKDAYS_ABBREVIATED
     
     #Here I will add the first week line
-    individual_lines << self.print_first_line_of_dates
+    individual_lines << first_line_of_dates
     
-    day_of_the_month = 8 - self.week_day_of_first_of_month # This will begin w/ the first day of 2nd week of the month
-    single_line = String.new
-    k = 1 # "k" will be incremented through the loop, & will be used to determine when breaks are needed
-    while day_of_the_month <= number_of_days_in_given_month
-      single_line += day_of_the_month.to_s.rjust(2)
-      single_line += " " if (k % 7 != 0 && day_of_the_month != number_of_days_in_given_month)
-      
-      if (k % 7 == 0 || day_of_the_month == number_of_days_in_given_month) # End of the week or month. Insert the string into the array and reset the string
-        individual_lines << single_line.ljust(line_length) # Insert the sting into the array.
-        single_line = String.new # Reset the string.
-      end
-      
-      day_of_the_month += 1
-      k += 1
-    end
+    day_of_the_month = 8 - weekday_of_1st_of_month # This will begin w/ the first day of 2nd week of the month
+    single_line = ""
+    k = 1 # "k" will be incremented through the loop, & will be used to determine when breaks are needed    
+    while day_of_the_month <= number_days_in_a_month
+         single_line += day_of_the_month.to_s.rjust(2)
+         single_line += " " if (k % 7 != 0 && day_of_the_month != number_days_in_a_month)         
+         if (k % 7 == 0 || day_of_the_month == number_days_in_a_month) # End of the week or month. Insert the string into the array and reset the string
+           individual_lines << single_line.ljust(NUM_CHARS_IN_LINE) # Insert the sting into the array.
+           single_line = "" # Reset the string.
+         end
+         day_of_the_month += 1
+         k += 1
+       end
     return individual_lines
   end
   
   def finish_array
-    almost_finished_array = self.build_array_of_individual_lines
-    month_and_year = self.textual_representation_of_the_month + " " + @year.to_s
-    finished_array = almost_finished_array.insert(0, month_and_year.center(20))
-    if @year == 1999 # This is an 'easter egg' (see http://en.wikipedia.org/wiki/Easter_egg_(media) fro more info)
-      finished_array.insert(1, "We're going to party like it's 1999".center(20))      
+    month_and_year = month_name_text + " " + @year.to_s
+    finished_array = build_array_of_individual_lines.insert(0, month_and_year.center(NUM_CHARS_IN_LINE))
+    if @year == 1999 # This is an 'easter egg' (see http://en.wikipedia.org/wiki/Easter_egg_(media) for more info)
+      finished_array.insert(1, "We're going to party like it's 1999".center(NUM_CHARS_IN_LINE))      
     end
     return finished_array
   end
